040f07438a4a4182b848e0f521d752fcf835c29a
JAMES FRANK MENDOZA RIOS <21100654@ue.edu.pe>
2025-08-30 03:11:33 -0500

    Agregar scripts de predespliegue y despliegue en package.json; ajustar la ruta base en vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/EnsenaPe',
+  base: '/EnsenaPe/',
   plugins: [react()],
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
 })
